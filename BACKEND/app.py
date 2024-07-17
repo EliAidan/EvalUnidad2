987332bfc689f6df9dd25db1b7963ba587ef2103
Eli <210538@utxicotepec.edu.mx>
2024-07-17 13:42:01 -0600

    cambios para inserccion

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
-from routes.persona import persona
 from routes.users import user
-from routes.persons import person
+from routes.person import person
+from routes.rol import rol
+from routes.usersrols import userrol
 
-app = FastAPI()
-
+app=FastAPI(
+    title="Hospital Privilage Care",
+    description="API para el almacenamiento de informacipn de un hospital"
+)
 app.include_router(user)
 app.include_router(person)
-
-
-print("Bienvenido a mi aplicacion")
+app.include_router(rol)
+app.include_router(userrol)
+print ("Hola bienvenido a mi backend")
